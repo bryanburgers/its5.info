9b401eeae830176557786d367624b61ff0440b80
Bryan Burgers <bryan@burgers.io>
2014-04-28 12:22:22 -0500

    Find duplicates for a designator

--- dups.js
+++ dups.js
@@ -1,32 +1,34 @@
 "use strict";
 
 var moment = require('moment-timezone');
+
+if (process.argv.length < 3) {
+	console.log("For what timezone should we find duplicates?");
+	process.exit(1);
+}
+
+var designator = process.argv[2];
+
 var zones = moment.tz.zones();
+var selectedZone = zones.filter(function(zone) { return zone.displayName == designator });
+if (selectedZone.length == 0) {
+	console.log("Could not find zone " + designator);
+	process.exit(2);
+}
+selectedZone = selectedZone[0];
+var selectedZoneInfo = getZoneInfoString(selectedZone);
 
-var things = {
-};
-
-zones = zones.filter(function(zone) { return /^America\//.test(zone.displayName); });
 zones.forEach(function(zone) {
-	var lastZone = zone.zones[zone.zones.length - 1];
+	var info = getZoneInfoString(zone);
 
+	if (info == selectedZoneInfo) {
+		console.log(zone.displayName);
+	}
+});
+
+function getZoneInfoString(zone) {
+	var lastZone = zone.zones[zone.zones.length - 1];
 	var ruleSetName = lastZone.ruleSet.name;
 	var offset = lastZone.offset.toString();
-
-	var key = ruleSetName + '/' + offset;
-	if (!things[key]) {
-		things[key] = [];
-	}
-
-	things[key].push(zone.displayName);
-});
-
-Object.keys(things).forEach(function(key) {
-	var items = things[key];
-	if (items.length > 1) {
-		console.log(key);
-		items.forEach(function(item) {
-			console.log('   ' + item);
-		});
-	}
-});
+	return ruleSetName + '/' + offset;
+}
