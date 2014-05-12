095be9a2efa72c1d7c41929481fecc7bc040d0c6
Bryan Burgers <bryan@burgers.io>
2014-05-12 18:47:36 -0500

    Cache some API calls to Twitter

--- tw.js
+++ tw.js
@@ -1,11 +1,16 @@
 "use strict";
 
+var LRU = require('lru-cache');
 var Q = require('q');
 var twitter = require('twitter');
 
 function TwitterApi(creds) {
 	this.creds = creds;
 	this.twit = new twitter(creds);
+	this.cache = LRU({
+		max: 20,
+		maxAge: 60 * 60 * 1000
+	});
 }
 
 TwitterApi.prototype._get = function(uri, options) {
@@ -21,16 +26,32 @@ TwitterApi.prototype._get = function(uri, options) {
 	});
 };
 
+TwitterApi.prototype._getCached = function(key, uri, options) {
+	var self = this;
+	if (self.cache.has(key)) {
+		return Q.Promise(function(resolve) {
+			resolve(self.cache.get(key));
+		});
+	}
+	else {
+		var promise = self._get(uri, options);
+		promise.then(function(data) {
+			self.cache.set(key, data);
+		});
+		return promise;
+	}
+};
+
 TwitterApi.prototype.search = function(term) {
 	return this._get('/search/tweets.json', { q: term, result_type: 'recent', count: 100 });
 };
 
 TwitterApi.prototype.oembed = function(id) {
-	return this._get('/statuses/oembed.json', { id: id, align: 'center' });
+	return this._getCached('oembed:' + id, '/statuses/oembed.json', { id: id, align: 'center' });
 };
 
 TwitterApi.prototype.getTweet = function(id) {
-	return this._get('/statuses/show.json', { id: id });
+	return this._getCached('tweet:' + id, '/statuses/show.json', { id: id });
 };
 
 module.exports = function(creds) {
